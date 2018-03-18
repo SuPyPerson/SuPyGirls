f3fa8da3a9614fc55e3f11d7dbd1560c7b06c035
Carlo E. T. Oliveira <carlo@nce.ufrj.br>
2018-03-17 23:31:33 -0300

    Error Dialog almost working; mistery of reopening dialog

--- kwarwpd/src/kwarwp/brython_factory.py
+++ kwarwpd/src/kwarwp/brython_factory.py
@@ -22,7 +22,8 @@
 .. moduleauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
 
 """
-
+import sys
+import traceback
 from random import choice
 
 from .kuarup import Kuarup
@@ -32,6 +33,12 @@ IMAGEREPO = 'server_root/image/'
 CANVASW, CANVASH = 800, 600
 NODICT = {}
 EDIT = "{}/{}".format(IMAGEREPO, "sun.gif")
+EDTST = {'position': 'relative', 'padding': 10, 'margin': '0', 'flex': '3 1 auto',
+         'width': '99%', 'resize': 'none', 'borderColor': 'darkslategrey',
+         'color': 'navajowhite', 'border': 1, 'background': 'rgba(10, 10, 10, 0.5)'}
+ERRST = {'position': 'relative', 'padding': 10, 'margin': '0', 'visibility': 'visible', 'flex': '1',
+         'width': '99%', 'height': '34%', 'resize': 'none', 'borderColor': 'darkslategrey',
+         'color': 'navajowhite', 'border': 1, 'background': 'rgba(200, 54, 54, 0.5)'}
 
 #############################################################################
 
@@ -90,19 +97,42 @@ DIALOGS = [('black', 20, 80, 760, 500), ('red', 20, 380, 760, 200)]
 
 class Dialog:
     def __init__(self, gui, text='xxxx', act=lambda x: None, kind=0):
+
+        divat = {'position': 'absolute', 'top': 64, 'left': 0, 'display': 'flex', 'padding': '1%',
+                 'flex-direction': 'column', 'align-items': 'stretch',
+                 'width': '800px', 'height': '536px', 'background': 'rgba(10, 10, 10, 0.85)'}
         background, *dimensions = DIALOGS[kind]
         self.text = text
-        self._rect = gui.back(0, 66, 800, 540, background, '0.85')
-        self._area = gui.textarea(text, 20, 80, 760, 500)
+        self.html, self.dom = gui.html, gui.dom
+        self._div = self._err = self._area = None
+        self._div = self._div if self._div else self.html.DIV(style=divat)
+        self.dom <= self._div
+        # self._rect = gui.back(0, 66, 800, 540, background, '0.85')
+        text = text if text else self.text
+        self._area = self.textarea(text, style=EDTST)
+        self._set_code(text)
+        # self.set_err(text)
         self.edit = gui.edit
         gui.continua, self.continua = lambda *_: None, gui.continua
         self.gui = gui
         gui.edit = self.action
         self.act = act
 
+    def _set_code(self, text=None):
+        self._div <= self._area
+
+    def textarea(self, text, style=EDTST):
+
+        def dpx(d):
+            return '%spx' % d
+        # divat = {'position': 'absolute', 'top': dpx(y), 'left': dpx(x),
+        #          'width': dpx(w), 'height': dpx(h), 'background': 'rgba(10, 10, 10, 0.85)'}
+        t = self.html.TEXTAREA(text, style=style)
+        return t
+
     def remove(self):
-        self._rect.remove()
-        self._area.remove()
+        self._div.remove()
+        # self._area.remove()
 
     def hide(self):
         self.remove()
@@ -110,11 +140,27 @@ class Dialog:
         # self._area.style.visibility = 'hidden'
 
     def show(self):
-        self._rect.style.visibility = 'visible'
+        # self._rect.style.visibility = 'visible'
+        self._set_code()
         self._area.style.visibility = 'visible'
+        self._div.style.visibility = 'visible'
+
+    def _update_text(self):
+        self.text = self._area.value
+        return self.text if self.text else self._update_text()
 
     def get_text(self):
-        return self.text
+        return self.text if self.text else self._update_text()
+
+    def set_err(self, text):
+        self._err.remove() if self._err else None
+        self._err = self.textarea(text, style=ERRST)
+        self._div <= self._err
+        self.text = ''
+
+    def del_err(self):
+        self._err.remove() if self._err else None
+        self._err = None
 
     def set_text(self, text):
         self._area.value = text
@@ -253,20 +299,20 @@ class _GUI:
     def escolha(self, lista):
         return choice(lista)
 
-    def textarea(self, text, x, y, w, h, style=NODICT):
+    def textarea(self, text, x, y, w, h, style=EDTST):
 
         def dpx(d):
             return '%spx' % d
 
-
-
-        attrs = {'position': 'relative', 'padding': 10, 'margin': 10,
-                 'width': '100%', 'height': '100%', 'resize': 'none', 'borderColor': 'darkslategrey',
-                 'color': 'navajowhite', 'border': 1, 'background': rgba(200, 54, 54, 0.5)}
-        divat = {'position': 'absolute', 'top': dpx(y), 'left': dpx(x),
-                 'width': dpx(w), 'height': dpx(h), 'background': 'transparent'}
+        attrs = {'position': 'relative', 'padding': 10, 'margin': "2%",
+                 'width': '96%', 'height': '100%', 'resize': 'none', 'borderColor': 'darkslategrey',
+                 'color': 'navajowhite', 'border': 1, 'background': 'rgba(200, 54, 54, 0.5)'}
+        divat = {'position': 'absolute', 'top': 64, 'left': 0,
+                 'width': '800px', 'height': '536px', 'background': 'rgba(10, 10, 10, 0.85)'}
+        # divat = {'position': 'absolute', 'top': dpx(y), 'left': dpx(x),
+        #          'width': dpx(w), 'height': dpx(h), 'background': 'rgba(10, 10, 10, 0.85)'}
         d = self.html.DIV(style=divat)
-        t = self.html.TEXTAREA(text, style=attrs)
+        t = self.html.TEXTAREA(text, style=style)
         d <= t
         self.dom <= d
         return t
@@ -306,40 +352,45 @@ class GUI(_GUI):
     def _render(self, img, render=None):
         self.queue.push(lambda: render() if render else self.do_render(img))
 
-    def _first_response(self, action):
-        value = self.value = cons_out()
+    def _first_response(self, dialog):
+        class ConsoleOutput:
+
+            def __init__(self):
+                self.value = ''
+
+            def write(self, data):
+                self.value += str(data)
+
+            def flush(self):
+                self.value = ''
+                pass
+
+        value = self.value = ConsoleOutput()
         sys_out, sys.stdout = sys.stdout, value
         sys_err, sys.stderr = sys.stderr, value
         # logger('first response %s %s %s' % (dialog, sys.stdout, sys.stderr))
         # TODO action += self.challenge[1]
         # logger('first response code %s' % action)
         try:
-            exec(action)
+            exec(self.cena, globals())
             pass
         except Exception as err:
-            # logger('first response error %s' % self.value.value)
-            self.challenge[0] = dialog.get_text()
-            dialog.set_text(self.value.value)
-            self._response = self._second_response
-            self.place.talk('Something went wrong in your attempt!!')
-            dialog.show()
+            traceback.print_exc(file=sys.stderr)
+            dialog = self.dialog(self.cena)  # +str(self.value.value))
+            dialog.set_err(str(self.value.value))
         else:
-            logger('first response else %s' % self.world.plan[0][0])
-            self.challenge[0] = dialog.get_text()
-            self.move(self.world.plan[0][0])
-            self.place.talk('It looks like you did it!!')
-            self._response = self._first_response
+            self.cena = dialog.get_text()
+            o_indio = Tchuk(Kuarup.CORREDOR_ROCHOSO, indio=Tchuk, gui=self)
+            o_indio.inicia()
+            pass
         sys.stdout = sys_out
         sys.stderr = sys_err
-        logger('first response value error %s' % self.value.value)
 
     def executa_acao(self, dialog):
         self.cena = dialog.get_text()
         self.renderer = self._render
         self.renderer = self.do_render
-        exec(self.cena, globals())
-        o_indio = Tchuk(Kuarup.CORREDOR_ROCHOSO, indio=Tchuk, gui=self)
-        o_indio.inicia()
+        self._first_response(dialog)
 
     def registra_executante(self, executante):
         self.renderer = self._render
