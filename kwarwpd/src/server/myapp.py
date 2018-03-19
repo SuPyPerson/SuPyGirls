186b14db5ba51a5e0306253afd9a62c9a5aeeb8d
cetoli <carlo@ufrj.br>
2018-03-19 11:12:58 -0300

    Deploy in cloud, still docker missing for issue #26

---
+++ kwarwpd/src/server/myapp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from bottle import route, run, default_app, get, static_file
+import os
+project_server = os.getcwd()
+# make sure the default templates directory is known to Bottle
+templates_dir = os.path.join(project_server, '../../src')
+js_dir = os.path.join(project_server, '../server_root/stlib')
+img_dir = os.path.join(project_server, '../server_root/image')
+print(templates_dir)
+
+
+@route('/')
+def index():
+    return static_file("kwarwp.html", root=templates_dir)  # "Hello from bottle with Python3 !"
+
+
+# Static Routes
+@get("<filepath:re:.*\.py>")
+def css(filepath):
+    return static_file(filepath, root=templates_dir)
+
+
+# Static Routes
+@get("/server_root/stlib/<filepath:re:.*\.(js|css)>")
+def js(filepath):
+    return static_file(filepath, root=js_dir)
+
+
+# Static Routes
+@get("<filepath:re:.*\.(js|css)>")
+def js(filepath):
+    return static_file(filepath, root=js_dir)
+
+
+# Static Routes
+@get("/server_root/image/<filepath:re:.*\.(png|jpg|svg|gif)>")
+def img(filepath):
+    return static_file(filepath, root=img_dir)
+
+
+if __name__ == "__main__":
+    run(host="localhost", port=8081)
+else:
+    application = default_app()
